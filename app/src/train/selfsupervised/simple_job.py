82ac3a65cf85fbd3a6ed769fc31aca80447c8b3b
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-05-16 19:36:21 -0400

    Create simple_job.py

---
+++ app/src/train/selfsupervised/simple_job.py
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#SBATCH --time=0-10:00:00
+#SBATCH --account=def-sh1352
+#SBATCH --mem=32000M            # memory per node
+#SBATCH --nodes=2                # total number of nodes (N to be defined)
+#SBATCH --gpus-per-node=1       # # number of GPUs reserved per node (here 1)
+#SBATCH --cpus-per-task=8      # CPU cores/threads
+#SBATCH --output=landcover.out
+#SBATCH --tasks-per-node=1
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+echo "Hello World"
+nvidia-smi
+
+# Load needed python and cuda modules
+module load python/3.11 cuda cudnn gdal libspatialindex
+
+#srun --ntasks $SLURM_NNODES --tasks-per-node=1 bash << EOF
+#virtualenv --no-download $SLURM_TMPDIR/env
+#source $SLURM_TMPDIR/env/bin/activate
+
+#pip install --no-index --upgrade pip
+#pip install --no-index torchgeo tensorboard
+#pip install lightly
+#EOF
+
+source ~/royenv/bin/activate
+
+echo 'hello'
+
+logdir=/home/karoy84/scratch/logs
+datadir=/home/karoy84/scratch/data
+
+export NCCL_BLOCKING_WAIT=1
+
+tensorboard --logdir=${logdir}/lightning_logs --host 0.0.0.0 --load_fast false & \
+    srun python ~/scratch/landcover-ssl/app/src/train/selfsupervised/train.py \
+    --batch_size 256 \
+    --epoch 2 \
+    --gpus_per_node 1 \
+    --number_of_nodes 2 \
+    --num_workers 8 \
+    --logdir ${logdir} \
+    --data_dir  ${datadir}
