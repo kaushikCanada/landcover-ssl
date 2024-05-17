cfcfe30f69451ab0600f07bdb9297300549198fe
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-05-17 00:27:35 -0400

    Update simple_job.sh

--- app/src/train/selfsupervised/simple_job.sh
+++ app/src/train/selfsupervised/simple_job.sh
@@ -27,7 +27,7 @@ export NCCL_BLOCKING_WAIT=1
 tensorboard --logdir=${logdir}/lightning_logs --host 0.0.0.0 --load_fast false & \
 python ~/scratch/landcover-ssl/app/src/train/selfsupervised/train.py \
     --batch_size 200 \
-    --epoch 2 \
+    --epoch 20 \
     --gpus_per_node 1 \
     --number_of_nodes 1 \
     --num_workers 20 \
