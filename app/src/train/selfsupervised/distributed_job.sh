3f08b45873a23e5fdd07ab47c77d3730fc0f53e3
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-05-17 19:43:36 -0400

    Update distributed_job.sh

--- app/src/train/selfsupervised/distributed_job.sh
+++ app/src/train/selfsupervised/distributed_job.sh
@@ -2,7 +2,8 @@
 #SBATCH --nodes 1             
 #SBATCH --gres=gpu:2          # Request 2 GPU "generic resources”.
 #SBATCH --tasks-per-node=2   # Request 1 process per GPU. You will get 1 CPU per process by default. Request more CPUs with the "cpus-per-task" parameter to enable multiple data-loader workers to load data in parallel.
-#SBATCH --mem=8G      
+#SBATCH --mem=8G     
+#SBATCH --account=def-sh1352
 #SBATCH --time=0-03:00
 #SBATCH --output=%N-%j.out
 
