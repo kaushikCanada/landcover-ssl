4c7f40d30b46ae97fbb3faf2a0b170d0554ae654
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-05-17 18:56:58 -0400

    Update distributed_job.sh

--- app/src/train/selfsupervised/distributed_job.sh
+++ app/src/train/selfsupervised/distributed_job.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
+#SBATCH --nodes 1             
+#SBATCH --gres=gpu:2          # Request 2 GPU "generic resources”.
+#SBATCH --tasks-per-node=2   # Request 1 process per GPU. You will get 1 CPU per process by default. Request more CPUs with the "cpus-per-task" parameter to enable multiple data-loader workers to load data in parallel.
+#SBATCH --mem=8G      
+#SBATCH --time=0-03:00
+#SBATCH --output=%N-%j.out
 
+module load python # Using Default Python version - Make sure to choose a version that suits your application
+srun --tasks-per-node=1 bash << EOF
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install torchvision --no-index
+EOF
+
+export NCCL_BLOCKING_WAIT=1  #Set this environment variable if you wish to use the NCCL backend for inter-GPU communication.
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+echo "MASTER_ADDR="$MASTER_ADDR
+
+echo "r$SLURM_NODEID master: $MASTER_ADDR"
+echo "r$SLURM_NODEID Launching python script"
+
+export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
+echo "WORLD_SIZE="$WORLD_SIZE
+
+# The $((SLURM_NTASKS_PER_NODE * SLURM_JOB_NUM_NODES)) variable tells the script how many processes are available for this execution. “srun” executes the script <tasks-per-node * nodes> times
+
+srun python pytorch-ddp-test.py --init_method tcp://$MASTER_ADDR:3456 --batch_size 256
