b39cffbe0f8e4d9ab2ac5393959a90a3b0a10dc7
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-05-17 23:00:39 -0400

    Update ddp-train.py

--- app/src/train/selfsupervised/ddp-train.py
+++ app/src/train/selfsupervised/ddp-train.py
@@ -78,76 +78,101 @@ parser.add_argument("--resume_from_checkpoint",
                              "None --> Do not use pre-trained model. Training will start from random initialized model", default=None)
 
 def main():
-    print("Starting...")
+	print("Starting...")
+	
+	args = parser.parse_args()
+	ngpus_per_node = torch.cuda.device_count()
+	dict_args = vars(args)
 
-    args = parser.parse_args()
-
-    ngpus_per_node = torch.cuda.device_count()
-
-    """ This next line is the key to getting DistributedDataParallel working on SLURM:
+	cleaned_all_gta_256m_path = dict_args['data_dir'] + "/AZURE/cleaned_all_gta_256m/"
+	cleaned_all_montreal_256m_path = dict_args['data_dir'] + "/AZURE/cleaned_all_montreal_256m/"
+	cleaned_gta_labelled_256m_path = dict_args['data_dir'] + "/AZURE/cleaned_gta_labelled_256m/"
+	output_path = dict_args['log_dir']
+	
+	""" This next line is the key to getting DistributedDataParallel working on SLURM:
 		SLURM_NODEID is 0 or 1 in this example, SLURM_LOCALID is the id of the 
- 		current process inside a node and is also 0 or 1 in this example."""
-
-    local_rank = int(os.environ.get("SLURM_LOCALID")) 
-    rank = int(os.environ.get("SLURM_NODEID"))*ngpus_per_node + local_rank
-
-    current_device = local_rank
-
-    torch.cuda.set_device(current_device)
-
-    """ this block initializes a process group and initiate communications
+		current process inside a node and is also 0 or 1 in this example."""
+	
+	local_rank = int(os.environ.get("SLURM_LOCALID")) 
+	rank = int(os.environ.get("SLURM_NODEID"))*ngpus_per_node + local_rank
+	
+	current_device = local_rank
+	
+	torch.cuda.set_device(current_device)
+	
+	""" this block initializes a process group and initiate communications
 		between all processes running on all nodes """
+	
+	print('From Rank: {}, ==> Initializing Process Group...'.format(rank))
+	#init the process group
+	dist.init_process_group(backend=args.dist_backend, init_method=args.init_method, world_size=args.world_size, rank=rank)
+	print("process group ready!")
+	
+	print('From Rank: {}, ==> Making model..'.format(rank))
+	
+	class Net(nn.Module):
+	
+	def __init__(self):
+	  super(Net, self).__init__()
+	
+	  self.conv1 = nn.Conv2d(3, 6, 5)
+	  self.pool = nn.MaxPool2d(2, 2)
+	  self.conv2 = nn.Conv2d(6, 16, 5)
+	  self.fc1 = nn.Linear(16 * 5 * 5, 120)
+	  self.fc2 = nn.Linear(120, 84)
+	  self.fc3 = nn.Linear(84, 10)
+	
+	def forward(self, x):
+	  x = self.pool(F.relu(self.conv1(x)))
+	  x = self.pool(F.relu(self.conv2(x)))
+	  x = x.view(-1, 16 * 5 * 5)
+	  x = F.relu(self.fc1(x))
+	  x = F.relu(self.fc2(x))
+	  x = self.fc3(x)
+	  return x
+	
+	net = Net()
+	
+	net.cuda()
+	net = torch.nn.parallel.DistributedDataParallel(net, device_ids=[current_device])
+	
+	print('From Rank: {}, ==> Preparing data..'.format(rank))
 
-    print('From Rank: {}, ==> Initializing Process Group...'.format(rank))
-    #init the process group
-    dist.init_process_group(backend=args.dist_backend, init_method=args.init_method, world_size=args.world_size, rank=rank)
-    print("process group ready!")
+	toronto_unlabelled_dataset = Worldview3UnlabelledDataset(root = cleaned_all_gta_256m_path
+                                                             ,transforms=CustomMultiViewTransform(input_size = 256,normalize=WORLDVIEW3_NORMALIZE)
+                                                             )
+	print(len(toronto_unlabelled_dataset))
+	# print(toronto_unlabelled_dataset[0])
+	
+	montreal_unlabelled_dataset = Worldview3UnlabelledDataset(root = cleaned_all_montreal_256m_path
+							      ,transforms=CustomMultiViewTransform(input_size = 256,normalize=WORLDVIEW3_NORMALIZE)
+							      )
+	print(len(montreal_unlabelled_dataset))
+	# print(montreal_unlabelled_dataset[0])
+ 	dataset = torch.utils.data.ConcatDataset([toronto_unlabelled_dataset, montreal_unlabelled_dataset])
+    	print(len(dataset))
+	mysampler = torch.utils.data.distributed.DistributedSampler(dataset)
+	mydataloader = DataLoader(dataset, batch_size=dict_args['batch_size'], shuffle=(mysampler is None), num_workers=dict_args['num_workers'], sampler=mysampler)
 
-    print('From Rank: {}, ==> Making model..'.format(rank))
+	print(len(mydataloader))
+    	model = BarlowTwinsTask(model='resnet18',in_channels=11, batch_size = dict['batch_size'])
+	print(model)
 
-    class Net(nn.Module):
-
-       def __init__(self):
-          super(Net, self).__init__()
-
-          self.conv1 = nn.Conv2d(3, 6, 5)
-          self.pool = nn.MaxPool2d(2, 2)
-          self.conv2 = nn.Conv2d(6, 16, 5)
-          self.fc1 = nn.Linear(16 * 5 * 5, 120)
-          self.fc2 = nn.Linear(120, 84)
-          self.fc3 = nn.Linear(84, 10)
-
-       def forward(self, x):
-          x = self.pool(F.relu(self.conv1(x)))
-          x = self.pool(F.relu(self.conv2(x)))
-          x = x.view(-1, 16 * 5 * 5)
-          x = F.relu(self.fc1(x))
-          x = F.relu(self.fc2(x))
-          x = self.fc3(x)
-          return x
-
-    net = Net()
-
-    net.cuda()
-    net = torch.nn.parallel.DistributedDataParallel(net, device_ids=[current_device])
-
-    print('From Rank: {}, ==> Preparing data..'.format(rank))
-
-    transform_train = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-    dataset_train = CIFAR10(root='~/scratch/landcover-ssl/data', train=True, download=False, transform=transform_train)
-
-    train_sampler = torch.utils.data.distributed.DistributedSampler(dataset_train)
-    train_loader = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=(train_sampler is None), num_workers=args.num_workers, sampler=train_sampler)
-
-    criterion = nn.CrossEntropyLoss().cuda()
-    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
-
-    for epoch in range(args.max_epochs):
-
-        train_sampler.set_epoch(epoch)
-
-        train(epoch, net, criterion, optimizer, train_loader, rank)
+	transform_train = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	
+	dataset_train = CIFAR10(root='~/scratch/landcover-ssl/data', train=True, download=False, transform=transform_train)
+	
+	train_sampler = torch.utils.data.distributed.DistributedSampler(dataset_train)
+	train_loader = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=(train_sampler is None), num_workers=args.num_workers, sampler=train_sampler)
+	
+	criterion = nn.CrossEntropyLoss().cuda()
+	optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
+	
+	for epoch in range(args.max_epochs):
+	
+	train_sampler.set_epoch(epoch)
+	
+	# train(epoch, net, criterion, optimizer, train_loader, rank)
 
 def train(epoch, net, criterion, optimizer, train_loader, train_rank):
 
