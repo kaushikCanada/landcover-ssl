24cd2ec5d404c3d04c71a082fb5efdcd28781ae6
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-05-17 22:37:11 -0400

    Update ddp-train.py

--- app/src/train/selfsupervised/ddp-train.py
+++ app/src/train/selfsupervised/ddp-train.py
@@ -21,6 +21,8 @@ import argparse
 parser = argparse.ArgumentParser(description='cifar10 classification models, distributed data parallel test')
 parser.add_argument('--lr', default=0.1, help='')
 parser.add_argument('--batch_size', type=int, default=768, help='')
+parser.add_argument('--logdir', help='logdir for models and losses. default = .', default='./', type=str)
+parser.add_argument("--data_dir", type=str, help="path to data")
 parser.add_argument('--max_epochs', type=int, default=4, help='')
 parser.add_argument('--num_workers', type=int, default=0, help='')
 parser.add_argument('--dist-backend', default='nccl', type=str, help='')
