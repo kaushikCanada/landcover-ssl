d653b65818bd3240a01f3422a83cb60c51325527
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-05-23 11:38:45 -0400

    Update embedding_plot.py

--- app/src/train/selfsupervised/embedding_plot.py
+++ app/src/train/selfsupervised/embedding_plot.py
@@ -1,3 +1,22 @@
 import umap
 import matplotlib.pyplot as plt
 import numpy as np
+
+embeddings = []
+filenames = []
+
+# disable gradients for faster calculations
+model.eval()
+with torch.no_grad():
+    for i, (x, _, fnames) in enumerate(dataloader_test):
+        # move the images to the gpu
+        x = x.to(device)
+        # embed the images with the pre-trained backbone
+        y = model.backbone(x).flatten(start_dim=1)
+        # store the embeddings and filenames in lists
+        embeddings.append(y)
+        filenames = filenames + list(fnames)
+
+# concatenate the embeddings and convert to numpy
+embeddings = torch.cat(embeddings, dim=0)
+embeddings = embeddings.cpu().numpy()
