14e701f3879bf742c34c7302189896dfdab2bc18
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-05-27 13:14:43 -0400

    Update distributed_job.sh

--- app/src/train/selfsupervised/distributed_job.sh
+++ app/src/train/selfsupervised/distributed_job.sh
@@ -44,7 +44,7 @@ echo "WORLD_SIZE="$WORLD_SIZE
             
 srun python ~/scratch/landcover-ssl/app/src/train/selfsupervised/barlow_twins.py \
             --batch_size 1024 \
-            --epochs 2 \
+            --epochs 100 \
             --workers 10 \
             --checkpoint_dir ${log_dir} \
             --data_dir  ${data_dir}
