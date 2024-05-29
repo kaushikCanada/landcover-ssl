50d063fd9a73a3d6e1023c7ba1e13db6ec8e1454
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-05-29 13:00:00 -0400

    Update barlow_twins.py

--- app/src/train/selfsupervised/barlow_twins.py
+++ app/src/train/selfsupervised/barlow_twins.py
@@ -257,22 +257,20 @@ class BarlowTwins(nn.Module):
         self.bn = nn.BatchNorm1d(sizes[-1], affine=False)
 
     def forward(self, y1, y2):
-	h1: Tensor = self.backbone(y1)
-	h2: Tensor = self.backbone(y2)
-        z1 = self.projector(h1)
-        z2 = self.projector(h2)
-
-        # empirical cross-correlation matrix
-        c = self.bn(z1).T @ self.bn(z2)
-
-        # sum the cross-correlation matrix between all gpus
-        c.div_(self.args.batch_size)
-        torch.distributed.all_reduce(c)
-
-        on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
-        off_diag = off_diagonal(c).pow_(2).sum()
-        loss = on_diag + self.args.lambd * off_diag
-        return loss, h1
+	    h1: Tensor = self.backbone(y1)
+	    h2: Tensor = self.backbone(y2)
+	    z1 = self.projector(h1)
+	    z2 = self.projector(h2)
+	    
+	    # empirical cross-correlation matrix
+	    c = self.bn(z1).T @ self.bn(z2)
+	    # sum the cross-correlation matrix between all gpus
+	    c.div_(self.args.batch_size)
+	    torch.distributed.all_reduce(c)
+	    on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
+	    off_diag = off_diagonal(c).pow_(2).sum()
+	    loss = on_diag + self.args.lambd * off_diag
+	    return loss, h1
 
 
 if __name__ == '__main__':
