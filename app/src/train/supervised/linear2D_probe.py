55a59aae4c7188581daf753da8cc0c7aa09e8281
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-06-03 22:34:43 -0400

    Update linear2D_probe.py

--- app/src/train/supervised/linear2D_probe.py
+++ app/src/train/supervised/linear2D_probe.py
@@ -42,7 +42,7 @@ def main():
                 lr=lr,
                 patience=10,
             )
-            trainer = pl.Trainer(max_epochs=2, accelerator='gpu', devices=1, default_root_dir = dict_args['checkpoint_dir'])
+            trainer = pl.Trainer(max_epochs=2, accelerator="gpu", devices=[0], num_nodes=1, default_root_dir = dict_args['checkpoint_dir'])
             
             trainer.fit(model=task, train_dataloaders = dm.train_dataloader(), val_dataloaders = dm.val_dataloader())
 
