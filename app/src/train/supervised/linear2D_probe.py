d36768f55d73eb2f1fd8ba8b49a38a2e51c85b99
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-06-03 21:27:43 -0400

    Update linear2D_probe.py

--- app/src/train/supervised/linear2D_probe.py
+++ app/src/train/supervised/linear2D_probe.py
@@ -2,30 +2,47 @@ import pytorch_lightning as pl
 import torch
 from train_utils.trainer import MyModel
 from datamodule import Worldview3LabelledDataModule
+import argparse
 
-root= '../eodata/AZURE/cleaned_gta_labelled_256m/'
-batch_size = 4
-num_workers = 0
+parser = argparse.ArgumentParser(description='Worldview 3')
+parser.add_argument('--lr', default=0.001, help='Learning Rate')
+parser.add_argument("--data_dir", type=str, help="path to data")
+parser.add_argument('--max_epochs', type=int, default=4,  metavar='N', help='number of data loader workers')
+parser.add_argument('--batch_size', default=32, type=int, metavar='N', help='mini-batch size')
+parser.add_argument('--num_workers', type=int, default=0, metavar='N', help='number of data loader workers')
+parser.add_argument('--checkpoint_dir', default='./checkpoint/', type=Path, metavar='DIR', help='path to checkpoint directory')
 
-dm = Worldview3LabelledDataModule(
-            root=root,batch_size=batch_size,num_workers=num_workers
-        )
+def main():
+            print("Starting...")
 
-dm.setup("fit")
+            args = parser.parse_args()
+            dict_args = vars(args)
+            root = dict_args['data_dir'] + "/AZURE/cleaned_gta_labelled_256m/"
+            batch_size = dict_args['batch_size']
+            num_workers = dict_args['num_workers']
+            
+            dm = Worldview3LabelledDataModule(
+                        root=root,batch_size=batch_size,num_workers=num_workers
+                    )
+            
+            dm.setup("fit")
+            
+            net = MyModel()
+            
+            task = MyModel(
+                model="unet",
+                backbone="resnet50",
+                weights=None,
+                in_channels=11,
+                num_classes=8,
+                loss="ce",
+                ignore_index=None,
+                lr=dict_args['lr'],
+                patience=10,
+            )
+            trainer = pl.Trainer(max_epochs=2,accelerator='gpu')
+            
+            trainer.fit(model=task, train_dataloaders = dm.train_dataloader(), val_dataloaders = dm.val_dataloader())
 
-net = MyModel()
-
-task = MyModel(
-    model="unet",
-    backbone="resnet50",
-    weights=None,
-    in_channels=11,
-    num_classes=8,
-    loss="ce",
-    ignore_index=None,
-    lr=0.001,
-    patience=10,
-)
-trainer = pl.Trainer(max_epochs=2,accelerator='gpu')
-
-trainer.fit(model=task, train_dataloaders = dm.train_dataloader(), val_dataloaders = dm.val_dataloader())
+if __name__=='__main__':
+   main()
