0734a3bad5eb47068a53e775e7e85712602412e6
Kaushik Roy <kaushik.roy1984@gmail.com>
2024-06-22 22:43:29 -0400

    Update linear2D_probe.py

--- app/src/train/supervised/linear2D_probe.py
+++ app/src/train/supervised/linear2D_probe.py
@@ -1,5 +1,6 @@
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import CSVLogger
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 import torch
 from pathlib import Path
 from train_utils.trainer import MyModel
